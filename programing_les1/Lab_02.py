48a6a618827da9651ffb3106475bc62cdb90a5c2
nicob825 <nico.g.bruno@gmail.com>
2016-09-06 10:17:14 -0700

    Lab 2

---
+++ programing_les1/Lab_02.py
@@ -0,0 +1,14 @@
+l=8
+w=5
+recArea=l*w
+print("the area of your rectangle is", recArea)
+h=5
+L=8
+W=5
+recSurf=W*L+W*L+h*L+h*L+h*W+h*W
+print("the surface area of your rectangle is", recSurf)
+name="Genese Avenue"
+adress=9603
+city="San Diego, CA"
+zip=92103
+print(adress, name, city, zip)
