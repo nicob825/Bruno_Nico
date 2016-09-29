63b392344d305ad5f92bc18e7eddae61e9c2aff9
MrRobinette <richard.robinette@sduhsd.net>
2016-09-29 10:49:07 -0700

    Add files via upload

---
+++ Lesson_03/bmi.py
@@ -0,0 +1,5 @@
+x=input("this is calculator, press enter:")
+weight=int(input("Please enter what you weigh in pounds. "))
+height=int(input("Please enter your height in inches. "))
+bmi=(weight/(height*height))*703
+print("Your bmi is", bmi)
