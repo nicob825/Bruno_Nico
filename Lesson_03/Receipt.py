945d11a4da13b24b6afe4393c5a681e217cd0cce
nicob825 <nico.g.bruno@gmail.com>
2016-09-29 10:29:52 -0700

    Revert "lesson 4"
    This reverts commit f2a74c78c604b2703f111a4a4a7e75d454cd65d4.
    
    # Conflicts:
    #	Lesson_04/test.py

---
+++ Lesson_03/Receipt.py
@@ -0,0 +1,24 @@
++def format(item,price):
+ +    print("{:.<10}\t{:10.2f}".format(item,price))
+ +
+ +#input item1
+ +item1=input("Please enter item1:")
+ +price1=float(input("please enter the price:"))
+ +#input item2
+ +item2=input("Please enter item2:")
+ +price2=float(input("please enter the price:"))
+ +#input item3
+ +item3=input("Please enter item3:")
+ +price3=float(input("please enter the price:"))
+ +#calculate subtotal
+ +subtotal=price1+price2+price3
+ +tax=0.08*subtotal
+ +total=subtotal*tax
+ +print("<<<<<recipt>>>>>>")
+ +format(item1,price1)
+ +format(item2,price2)
+ +format(item3,price3)
+ +format("Subtotal:",subtotal)
+ +format("tax",tax)
+ +#same thing for tax, and total
+ +print("_______________________________________")
