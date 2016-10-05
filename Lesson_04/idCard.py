ffbdbef088c20c638cac9dfa59853820f787705e
nicob825 <nico.g.bruno@gmail.com>
2016-10-05 10:21:13 -0700

    id card

---
+++ Lesson_04/idCard.py
@@ -0,0 +1,8 @@
+def format(fname,lname,title,year,subject):
+    fname=input(format("Enter your first name:")
+    lname=input(format("Enter your last name:")
+    title=input(format("Enter your title:")
+    year=input(format("Enter your school year:")
+    subject=input(format("Enter your school subject:")
+
+
