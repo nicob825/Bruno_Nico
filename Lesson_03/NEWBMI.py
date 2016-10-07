a7f8e27d0a3dfa025f0877ecd746a1e5af4a4096
nicob825 <nico.g.bruno@gmail.com>
2016-10-07 10:24:16 -0700

    lesson 4

--- Lesson_03/NEWBMI.py
+++ Lesson_03/NEWBMI.py
@@ -1,4 +1,4 @@
 weight=float(input("Enter your weight:"))
 height=float(input("Enter your height:"))
 BMI=(weight*0.45/(height*0.025+height*0.025)
-print("The BMI calculated and it is",BMI)
+     ("The BMI calculated and it is",BMI)
--- Lesson_04/idCard.py
+++ Lesson_04/idCard.py
@@ -1,8 +1,20 @@
-def format(fname,lname,title,year,subject):
-    fname=input(format("Enter your first name:")
-    lname=input(format("Enter your last name:")
-    title=input(format("Enter your title:")
-    year=input(format("Enter your school year:")
-    subject=input(format("Enter your school subject:")
-print("{
+def formater(firstname,lastname,title,year,subject,site):
+    print("{:*<35}")
+    print("*{:>35}{:>35}".format(site, year))
+    print("*{:>35}{:>35}".format(firstname, lastname))
+    print("*{:>35}{:>35}".format(title, subject))
+    print("{:*<35}")
+
+site=input(format("Enter your school site:")
+firstname=input(format("Enter your first name:")
+lastname=input(format("Enter your last name:")
+title=input(format("Enter your title:")
+year=input(format("Enter your school year:")
+subject=input(format("Enter your school subject:")
+
+
+
+
+
+
 
