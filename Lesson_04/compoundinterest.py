03ec40a8622a15b99bcbaed0e69b27daa82e152d
nicob825 <nico.g.bruno@gmail.com>
2016-10-11 10:07:01 -0700

    lesson 4

--- Lesson_04/compoundinterest.py
+++ Lesson_04/compoundinterest.py
@@ -6,6 +6,8 @@ P=float(input("Enter the principal:"))
 n=float(input("Enter the number of times the loan is compunded per year:"))
 t=float(input("Enter the life of the loan in years:"))
 
-print("The interest of your loan is {:0.2f}".format(calcPayment(r, P, n, t))
-      
+print("The interest of your loan is {:0.2f}".format(calcPayment(r, P, n, t)))
+
+
+
 
