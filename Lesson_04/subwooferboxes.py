fa39b1c81c86190209afecac6a3fd602e737aedf
nicob825 <nico.g.bruno@gmail.com>
2016-10-12 10:24:02 -0700

    lesson 4

--- Lesson_04/subwooferboxes.py
+++ Lesson_04/subwooferboxes.py
@@ -1,4 +1,10 @@
 def calcVol(h,l,w):
-    return h*l*w
+    return (h*l*w)
+
+h=float(input("Enter the height:"))
+l=float(input("Enter the length:"))
+w=float(input("Enter the width:"))
+
+print("The cubic inches is {:0.2f} and the cubic feet is {:0.2f}".format(calcVol(h,l,w), calcVol(h,l,w)/1728))
 
 
