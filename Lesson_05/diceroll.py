afc16844be5fca4e9988533386e850cb5fc00148
nicob825 <nico.g.bruno@gmail.com>
2016-10-14 10:24:05 -0700

    lesson 5

--- Lesson_05/diceroll.py
+++ Lesson_05/diceroll.py
@@ -5,6 +5,8 @@ import random
 dice=random.randint(1,6)
 print("The value of your dice is:", dice)
 if dice>dice2:
+    print("You win! Computer loses!")
 if dice2>dice:
+    print("You lose! Computer wins!")
 if dice==dice2:
     print("Its a tie! No one wins!")
