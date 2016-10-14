9a262cecd5541abd6cfca4433d0ff95505299177
nicob825 <nico.g.bruno@gmail.com>
2016-10-14 10:23:54 -0700

    lesson 5

--- Lesson_05/diceroll.py
+++ Lesson_05/diceroll.py
@@ -1,2 +1,10 @@
 import random
-num=random.randint(1,6)
+dice2=random.randint(1,6)
+print("The value of the computers dice is:", dice2)
+import random
+dice=random.randint(1,6)
+print("The value of your dice is:", dice)
+if dice>dice2:
+if dice2>dice:
+if dice==dice2:
+    print("Its a tie! No one wins!")
---
+++ Lesson_05/discount.py
@@ -0,0 +1,10 @@
+item1=input("Enter your first item:")
+price1=float(input("Enter the price of item1:"))
+item2=input("Enter your second item:")
+price2=float(input("Enter the price of item2:"))
+item3=input("Enter your third item:")
+price3=float(input("Enter the price of item3:"))
+item4=input("Enter your fourth item:")
+price4=float(input("Enter the price of item4:"))
+
+item1+item2+item3+item4=total
