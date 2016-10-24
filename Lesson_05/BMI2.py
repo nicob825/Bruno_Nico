56bb5d8656eea16cd980af2cf9c690181ad3758e
nicob825 <nico.g.bruno@gmail.com>
2016-10-24 10:24:01 -0700

    Lesson 5

--- Lesson_05/BMI2.py
+++ Lesson_05/BMI2.py
@@ -1,2 +1,23 @@
 weight=int(input("Enter your weight:"))
 height=int(input("Enter your height in inches:"))
+bmi=0
+
+def calcBMI(BMI):
+    if BMI < 18.5:
+        return "Underweight"
+    elif BMI < 25:
+        return "Normal"
+    elif BMI < 30:
+        return "Overweight"
+    elif BMI < 35:
+        return "Obese"
+    elif BMI < 40:
+        return "Very Obese"
+    else:
+        return "Morbidly Obese"
+
+
+BMI=(703*weight)/(height**2)
+
+print("Your BMI is:",BMI)
+print("Your condition is:", calcBMI(BMI))
