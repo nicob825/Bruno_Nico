afe50befc78697cd2d8f04a4e132fcea87bc9779
nicob825 <nico.g.bruno@gmail.com>
2016-10-25 10:24:42 -0700

    lesson 5

---
+++ Lesson_05/Lesson_05.py
@@ -0,0 +1,30 @@
+print("You are in the woods, you don't know how you got there. You see a sword on the ground and a spell tome.")
+choice = input("What do you pick up, the sword or the spell tome?")
+
+if choice == "sword":
+    print("You have picked up the sword! It is even balanced and you can feel rage course through you. You put it back in the scabard and your rage feels bottled up.")
+    print("You stumble across a bandit camp! ")
+    choice = input("You can either sneak and take supplies you need or deliver punishment. Sneak or kill:")
+    if choice == "kill":
+        print("You kill the rest of them. You now notice the bandit leader who is twice as burly as you.")
+        choice = input("Do you try: reason or slay")
+        if choice=="slay":
+            print("As you point your sword to him he understands the threat. He pulls out a massive axe and charges you. You cleave him in half cleanly and you are victorious.")
+        else:
+            print("As you walk up to the man, he seems to twitch when you come to close. As you jump back, an axe misses your head only by centimeters. He charges you and you dodge the first attack and he lands a blow on you and you die.")
+    else:
+        if choice=="sneak":
+
+        else:
+
+else:
+    print("You picked up the spell...")
+    if choice==a:
+        if choice==b:
+
+        else:
+
+    else:
+        if choice==c:
+
+        else:
