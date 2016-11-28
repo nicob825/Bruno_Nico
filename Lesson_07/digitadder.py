9d99997fb3c06dcc57b3b467d769f9177ad11226
nicob825 <nico.g.bruno@gmail.com>
2016-11-28 10:23:21 -0800

    lesson 7

--- Lesson_07/digitadder.py
+++ Lesson_07/digitadder.py
@@ -1,7 +1,10 @@
 number=int(input("Enter the number:"))
 num=number
 sum=0
-while num > 0:
-    sum += 1
-    num = int(num / 10)
+def sumDigit():
+    while num > 0:
+        sum += 1
+        num = int(num / 10)
 print("The sum of the digits in",number," is",sum)
+
+sumDigit()
---
+++ Lesson_07/lesson_07.1.py
@@ -0,0 +1,6 @@
+sentence=input("Please enter a string:")
+top = 0
+while top < len(sentence):
+    print(sentence[top])
+    top += 1
+    
