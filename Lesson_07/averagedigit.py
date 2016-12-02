8a4f9844021eeffa102a906d2ded7dc2837f0527
nicob825 <nico.g.bruno@gmail.com>
2016-12-02 10:20:09 -0800

    lesson 7

--- Lesson_07/averagedigit.py
+++ Lesson_07/averagedigit.py
@@ -1,17 +1,14 @@
 def avDigits():
     global num
     global average
-    global digits
+    digits = len(str(number))
     while num > 0:
-        digits += num % 10
-        average = len(digits)
-
-
-
-
-
-
-digits=0
+        average += num % 10
+        num = int(num / 10)
+    average = average/digits
+        
 average=0
 number=int(input("Enter the number you want:"))
 num=number
+avDigits()
+print("The average of the digits in",number," is",average)
