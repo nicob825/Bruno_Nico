c9468a3dbfd613d71617b01d44c858352159a4e5
nicob825 <nico.g.bruno@gmail.com>
2016-12-08 10:23:40 -0800

    lesson 7

--- Lesson_07/replaceats.py
+++ Lesson_07/replaceats.py
@@ -1,4 +1,10 @@
-sentance=input("Please enter a sentence:")
+sentence=input("Please enter a sentence:")
+
 def replace():
-    while sentence,count(":
+    global sentence
+    while sentence.count("a") > 0:
+        sentence = sentence[0: sentence.index("a")] +"@" + sentence[sentence.index("a")+1 : len(sentence)]
+
+replace()
+print(sentence)
     
